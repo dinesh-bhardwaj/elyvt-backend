eca2056c6dffa3dba54e20359f7805afd6be922d
Rafhael Genio <rafhael@codeninja.co>
2018-01-18 19:38:16 +0800

    test

--- resources/functions.js
+++ resources/functions.js
@@ -916,11 +916,11 @@ getfolderTasks :function(moment, $tasksData, $contactsData,  $folderId){
 
     buildUpcomingTasksData(tasks) {
     	return tasks.reduce((acc, task) => {
-			if (task.dates.type !== 'backlog') {
-				acc[findProject(task.parentFolderIds[0]).title] = 
-					acc[findProject(task.parentFolderIds[0]).title] ?
-						acc[findProject(task.parentFolderIds[0]).title] + 1 : 1
-			}
+			// if (task.dates.type !== 'backlog') {
+			// 	acc[findProject(task.parentFolderIds[0]).title] = 
+			// 		acc[findProject(task.parentFolderIds[0]).title] ?
+			// 			acc[findProject(task.parentFolderIds[0]).title] + 1 : 1
+			// }
 
 			return [...acc, task];
 		}, [])
